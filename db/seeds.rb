24aa93c872230605ffb915d944d4623a80309896
Denis Kalesnikov <DenisKem@users.noreply.github.com>
2018-05-21 11:34:15 +0700

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Rails.logger.level = 3
 ips = (1..50).to_a.map { Faker::Internet.ip_v4_address  }
 authors = (1..100).to_a.map { Faker::Internet.user_name }
 
-200_000.times do
+200_000.times do |i|
   post = PostHandler.perform("author_login": "#{authors.sample}", "author_ip": "#{ips.sample}",
                              "content": "#{Faker::Lorem.paragraph}", "title": "#{Faker::Lorem.sentence}").post
 
@@ -13,6 +13,6 @@ authors = (1..100).to_a.map { Faker::Internet.user_name }
   end
 
   post.update_average_rate!
-end
 
-puts "#{i} done" if (i%10_000).zero?
+  puts "#{i} done" if (i%1_000).zero?
+end
