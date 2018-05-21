5631e80d8eac748e4cadacd76a6089474ac21089
DenisKem <denis.kem@gmail.com>
2018-05-21 10:06:52 +0700

    big data

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ group :development, :test do
 end
 
 group :development do
+  gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,18 @@
-puts `curl --header "Content-Type: application/json" \
-  --request POST \
-  --data '{"author_login":"test", "author_ip":"0.0.0.0", "content": "Hello, world!", "title": "Greetings"}' \
-  http://localhost:3000/api/v1/posts`
+Rails.logger.level = 3
+
+ips = (1..50).to_a.map { Faker::Internet.ip_v4_address  }
+authors = (1..100).to_a.map { Faker::Internet.user_name }
+
+200_000.times do
+  post = PostHandler.perform("author_login": "#{authors.sample}", "author_ip": "#{ips.sample}",
+                             "content": "#{Faker::Lorem.paragraph}", "title": "#{Faker::Lorem.sentence}").post
 
 
-Post.find_each do |post|
-  puts `curl --header "Content-Type: application/json" \
-  --request POST \
-  --data '{"value":"1"}' \
-  http://localhost:3000/api/v1/posts/#{post.id}/rate`
+  3.times do
+    post.rates.create!(value: rand(1..5))
+  end
+
+  post.update_average_rate!
 end
+
+puts "#{i} done" if (i%10_000).zero?
